2b816d450c9418ad562c12a1cac2d4dec8348b6c
Mirza Morales <natzllyuni@gmail.com>
2025-02-27 13:15:15 -0600

    Carpetas de componentes y bootstrap

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { UsuariosAdminComponent } from './components/administrador/usuarios-admin/usuarios-admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'usuarios-admin', component: UsuariosAdminComponent },
+  { path: '**', redirectTo: 'usuarios-admin' }
+];
---
+++ FrontEnd/src/app/components/administrador/usuarios-admin/usuarios-admin.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../../navbar/navbar.component';
+
+@Component({
+  selector: 'app-usuarios-admin',
+  standalone: true,
+  imports: [NavbarComponent],
+  templateUrl: './usuarios-admin.component.html',
+  styleUrl: './usuarios-admin.component.css'
+})
+export class UsuariosAdminComponent {
+
+}
