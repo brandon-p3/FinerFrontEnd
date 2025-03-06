8d46b1abf2d097f5b52b541bf88caebe3399a70e
Mirza Morales <natzllyuni@gmail.com>
2025-03-06 08:37:56 -0600

    routing module

---
+++ FrontEnd/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+    imports: [
+        AppRoutingModule,
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [
+        AppComponent
+    ]
+})
+export class AppModule { }
