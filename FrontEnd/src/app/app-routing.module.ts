6de292aacd5b4b0716334e8b47fc8942744a7605
Vanesa Hernández <vanehernan464775@gmail.com>
2025-03-06 13:27:51 -0600

    avance

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsuariosAdminComponent } from './components/administrador/usuarios-admin/usuarios-admin.component';
+import { LoginComponent} from './components/login/login/login.component';
+import { RegistroComponent } from './components/login/registro/registro.component';
 
 export const routes: Routes = [
-  { path: 'usuarios-admin', component: UsuariosAdminComponent },
-  { path: '**', redirectTo: 'usuarios-admin' }
+  { path: '', redirectTo: 'usuarios-admin', pathMatch: 'full' },
+
+  {
+    path: 'usuarios-admin',
+    component: UsuariosAdminComponent
+  },
+
+  { path: 'usuarios-admin/login/login',
+    component: LoginComponent
+   },
+
+  { path: 'usuarios-admin/registro',
+    component: RegistroComponent
+  }
 ];
 
 @NgModule({
