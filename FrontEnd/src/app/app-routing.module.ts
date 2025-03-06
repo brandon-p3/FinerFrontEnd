0b8cbee1ee44116d85b19e6f7dbbe5beebad579f
Mirza Morales <natzllyuni@gmail.com>
2025-03-06 10:30:53 -0600

    verCursos admin avance

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,14 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsuariosAdminComponent } from './components/administrador/usuarios-admin/usuarios-admin.component';
+import { CursosAdminComponent } from './components/administrador/cursos-admin/cursos-admin.component';
 
 export const routes: Routes = [
-  { path: 'usuarios-admin', component: UsuariosAdminComponent },
-  { path: '**', redirectTo: 'usuarios-admin' }
+  // Rutas para el administrador
+  {
+    path: 'administrador',
+    children: [
+      {
+        path: 'usuarios',
+        children: [
+          { path: 'ver', component: UsuariosAdminComponent }
+        ]
+      },
+      {
+        path: 'cursos',
+        children: [
+          { path: 'ver', component: CursosAdminComponent }
+        ]
+      }
+    ]
+  },
+
+  // Redirección por defecto 
+  { path: '**', redirectTo: 'administrador/usuarios/ver' }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
