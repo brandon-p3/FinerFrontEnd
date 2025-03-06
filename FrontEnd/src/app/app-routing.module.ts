ceec9b7e3b6bde93ec10550bd7ed8e573a850073
Chesita <rmm.che@gmail.com>
2025-03-06 12:56:18 -0600

    Agregar cambios

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsuariosAdminComponent } from './components/administrador/usuarios-admin/usuarios-admin.component';
+import { CursosAlumnosComponent } from './components/alumnos/cursos-alumnos/cursos-alumnos.component';
+import { ContenidoCursoComponent } from './components/alumnos/contenido-curso/contenido-curso.component';
+import { DescripcionCursoAlumnoComponent } from './components/alumnos/descripcion-curso-alumno/descripcion-curso-alumno.component';
 
 export const routes: Routes = [
   { path: 'usuarios-admin', component: UsuariosAdminComponent },
+  { path: 'cursos-alumnos', component: CursosAlumnosComponent},
+  { path: 'contenido-curso', component: ContenidoCursoComponent},
+  { path: 'descripcion-curso-alumno', component: DescripcionCursoAlumnoComponent},
   { path: '**', redirectTo: 'usuarios-admin' }
 ];
 
