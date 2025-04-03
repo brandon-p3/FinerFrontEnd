073a55529db8125ecf8af5e887e861a410c534af
carolross1 <115049387+carolross1@users.noreply.github.com>
2025-04-03 01:22:32 -0600

    Cambios en instructor/cursos

--- FrontEnd/src/app/services/categorias-service.service.ts
+++ FrontEnd/src/app/services/categorias-service.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { VerCategoriasDTO } from '../documentos/cursoDocumento';
-import { map } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,8 +12,11 @@ export class CategoriaServiceService {
   constructor(private http: HttpClient) { }
 
   obtenerCategoriasAprobadas(): Observable<VerCategoriasDTO[]> {
-    return this.http.get<any>(`${this.apiUrl}/ver-categoria-aprobada`).pipe(
-      map(response => response.data) // Extrae solo el array de categorías
+    return this.http.get<VerCategoriasDTO[]>(`${this.apiUrl}/ver-categoria-aprobada`).pipe(
+      catchError(error => {
+        console.error('Error al cargar categorías:', error);
+        return of([]); // Retorna un array vacío en caso de error
+      })
     );
   }
 
