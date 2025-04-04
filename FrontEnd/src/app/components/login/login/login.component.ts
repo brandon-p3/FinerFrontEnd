0b465ea60d787eb9641ee5b68dfab6d2d9f1dd0e
Mirza Morales <natzllyuni@gmail.com>
2025-04-03 20:32:49 -0600

    Login FUNCIONAL!!

--- FrontEnd/src/app/components/login/login/login.component.ts
+++ FrontEnd/src/app/components/login/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router'; 
+import { UsuariosService } from '../../../services/usuarios-service.service';
 
 @Component({
   selector: 'app-login',
@@ -8,8 +9,52 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
 
-  constructor(private router: Router) {} 
+  nombreUsuario: string = '';
+  contrasenia: string = '';
+  errorMensaje: string = '';
+
+  mostrarContrasena: boolean = false;
+
+  constructor(
+    private router: Router,
+    private usuariosService: UsuariosService
+  ) {} 
+
+  toggleMostrarContrasena(): void {
+    this.mostrarContrasena = !this.mostrarContrasena;
+  }
   
+  iniciarSesion(): void {
+    this.usuariosService.iniciarSesion(this.nombreUsuario, this.contrasenia).subscribe({
+      next: (response) => {
+        console.log('Sesión iniciada con éxito:', response);
+  
+        const idUsuario = response.idUsuario;
+        const idRol = response.idRol;
+  
+        // Guardar en localStorage
+        localStorage.setItem('idUsuario', idUsuario);
+        localStorage.setItem('idRol', idRol);
+  
+        // Redireccionar según el rol
+        if (idRol === 1) {
+          this.router.navigate(['/administrador/cursos/ver']);
+        } else if (idRol === 2) {
+          this.router.navigate(['/instructor/cursos']);
+        } else if (idRol === 3) {
+          this.router.navigate(['/alumnos/cursos']);
+        } else {
+          this.errorMensaje = 'Rol no reconocido. Comunícate con soporte.';
+        }
+      },
+      error: (error) => {
+        console.error('Error al iniciar sesión:', error);
+        this.errorMensaje = error.error?.error || 'Error desconocido al iniciar sesión';
+      }
+    });
+  }
+  
+
   navigateToRegister() {
     this.router.navigate(['/home/registro']); 
   }
--- FrontEnd/src/app/config/config.ts
+++ FrontEnd/src/app/config/config.ts
@@ -1,5 +1,6 @@
 export const CONFIG = {
     apiUrl: 'http://localhost:8080/api',
 
+    apiInicio: 'http://localhost:8080/api'
   };
   
